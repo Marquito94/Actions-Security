09911738b77cb1ae6c058516ca16a01db5d5ccbe
Marquito94 <71236169+Marquito94@users.noreply.github.com>
2023-04-13 20:29:27 -0300

    Update testbucket.tf

--- testbucket.tf
+++ testbucket.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "buckettest" {
-   name          = "test-bucket-marco-123"
-   location      = "US"
-   force_destroy = true
+  name          = "testbucketmarco123"
+  location      = "US"
+  force_destroy = true
 }
