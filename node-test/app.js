924fdcff514a281a6871dfff3aa5aa546ecf9062
Marquito94 <71236169+Marquito94@users.noreply.github.com>
2023-09-25 13:09:34 -0300

    Update app.js

--- node-test/app.js
+++ node-test/app.js
@@ -1,28 +1,73 @@
-const createError = require('http-errors')
 const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
-
-const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
+const bodyParser = require('body-parser')
+const notes = [{
+		noteId: 1,
+		noteContent: "Hey guys, add your important notes here."
+	}
+]
 
 const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+app.set('view engine', 'ejs')
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.get("/", function (req, res) {
+	res.render("home", {
+		data: notes
+	})
+})
 
-// catch 404 and forward to error handler
+app.post("/", (req, res) => {
+	const noteContent = req.body.noteContent
+	const noteId = notes.length + 1;
+
+	notes.push({
+		noteId: noteId,
+		noteContent: noteContent
+	})
+
+	res.render("home", {
+		data: notes
+	})
+})
+
+app.post('/update', (req, res) => {
+	var noteId = req.body.noteId;
+	var noteContent = req.body.noteContent;
+	
+	notes.forEach(note => {
+		if (note.noteId == noteId) {
+			note.noteContent = noteContent;
+		}
+	})
+	res.render("home", {
+		data: notes
+	})
+})
+
+app.post('/delete', (req, res) => {
+	var noteId = req.body.noteId;
+
+	var j = 0;
+	notes.forEach(note => {
+		j = j + 1;
+		if (note.noteId == noteId) {
+			notes.splice((j - 1), 1)
+		}
+	})
+
+	res.render("home", {
+		data: notes
+	})
+})
+
+app.listen(3000, (req, res) => {
+	console.log("App is running on port 3000")
+})
 app.use(function (req, res, next) {
   next(createError(404))
 })
